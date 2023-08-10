b2430ec4c9c0544fd78c1898e121ac4a534caada
k-impossible <kth3349@gmail.com>
2023-08-10 14:11:30 +0900

    FE 프로젝트 세팅 완료

---
+++ client/src/pages/AddQuestion/AddQuestion.js
@@ -0,0 +1,13 @@
+import Footer from '../../components/Footer/Footer';
+import { BaseContainer, BaseWrap } from '../../style/Global.styled';
+
+export default function AddQuestion() {
+  return (
+    <BaseContainer>
+      <BaseWrap>
+        <div>질문 생성 페이지</div>
+      </BaseWrap>
+      <Footer />
+    </BaseContainer>
+  );
+}
