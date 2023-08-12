37a94c7cc8a7cf62781f9806bff2560591c77599
origin <git@github.com:ehdals0405/PreProjectback.git>
2023-08-12 12:19:51 +0900

    ExJWT ception 추가

--- server/src/main/java/five/group/server/jwt/JwtVerificationFilter.java
+++ server/src/main/java/five/group/server/jwt/JwtVerificationFilter.java
@@ -1,6 +1,8 @@
 package five.group.server.jwt;
 
 import five.group.server.auth.MemberAuthority;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.SignatureException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
@@ -26,11 +28,20 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        Map<String, Object> claims = verifyJws(request);
-        setAuthenticationToContext(claims);
+        try {
+            Map<String, Object> claims = verifyJws(request);
+            setAuthenticationToContext(claims);
+        } catch (SignatureException sign) {
+            request.setAttribute("exception", sign);
+        } catch (ExpiredJwtException expire) {
+            request.setAttribute("exception", expire);
+        } catch (Exception e) {
+            request.setAttribute("exception", e);
+        }
 
-        filterChain.doFilter(request,response);
+        filterChain.doFilter(request, response);
     }
+
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
         String authorization = request.getHeader("Authorization");
@@ -39,19 +50,19 @@ public class JwtVerificationFilter extends OncePerRequestFilter {
 
     }
 
-
-    private Map<String, Object> verifyJws(HttpServletRequest request){
+    private Map<String, Object> verifyJws(HttpServletRequest request) {
         String jws = request.getHeader("Authorization").replace("Bearer ", "");
         String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
-        Map<String, Object> claims = jwtTokenizer.getClaims(jws,base64EncodedSecretKey).getBody();
+        Map<String, Object> claims = jwtTokenizer.getClaims(jws, base64EncodedSecretKey).getBody();
 
         return claims;
     }
-    private void setAuthenticationToContext(Map<String,Object> claims){
+
+    private void setAuthenticationToContext(Map<String, Object> claims) {
         String username = (String) claims.get("username");
         List<String> roles = (List<String>) claims.get("roles");
         List<GrantedAuthority> authorities = memberAuthority.createAuthorities(roles);
-        Authentication authentication = new UsernamePasswordAuthenticationToken(username,null,authorities);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 }
